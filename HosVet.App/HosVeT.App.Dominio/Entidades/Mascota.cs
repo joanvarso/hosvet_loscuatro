e159195eabbbfe3ced9c70606c17b5be8d8d244a
Estefanía Sierra Castillo <55397847+EstefaniaSierra@users.noreply.github.com>
2021-09-15 23:09:27 -0500

    Add files via upload

---
+++ HosVet.App/HosVeT.App.Dominio/Entidades/Mascota.cs
@@ -0,0 +1,18 @@
+using System;
+namespace HosVet.App.Dominio
+{
+    public class Mascota
+    {
+        
+    public int id {get;set}
+    public string Nombre {get;set}
+    public string Direccion {get;set}
+    public string Ciudad {get;set}
+    public int Latitud {get;set}
+    public int Longitud {get;set}
+    public string TipoMascota {get;set}
+    public string Raza {get;set}
+    public int Pesp {get;set}
+    
+    } 
+}
---
+++ HosVet.App/HosVeT.App.Persistencia/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace HosVeT.App.Persistencia
+{
+    public class Class1
+    {
+    }
+}
