caa1b3f9784bc89ecad3a4a8c4feb4dd8b8d3bdb
EDZA09 <73438117+EDZA09@users.noreply.github.com>
2021-09-16 09:55:20 -0500

    Update Mascota.cs

--- HosVet.App/HosVeT.App.Dominio/Entidades/Mascota.cs
+++ HosVet.App/HosVeT.App.Dominio/Entidades/Mascota.cs
@@ -1,18 +1,30 @@
 using System;
 namespace HosVet.App.Dominio
 {
+/// <summary>Class <c>Paciente</c>
+    /// Modela una Mascota que esta en atencion hospitalaria
+/// </summary> 
     public class Mascota
     {
         
+    // Número que identifica a la mascota
     public int id {get;set}
+    // Nombre de la mascota
     public string Nombre {get;set}
+    // Lugar de residencia de la mascota     
     public string Direccion {get;set}
+    // Ciudad de residencia de la mascota
     public string Ciudad {get;set}
+    /// Coordenada de la dirección de la mascota
     public int Latitud {get;set}
+    // Coordenada de la dirección de la mascota
     public int Longitud {get;set}
+    // Indica que tipo de mascota es, Un Gato o Perro
     public string TipoMascota {get;set}
+    // Indica a que raza pertenece el animal doméstico
     public string Raza {get;set}
-    public int Pesp {get;set}
+    // Peso de la mascota
+    public int Peso {get;set}
     
     } 
 }
