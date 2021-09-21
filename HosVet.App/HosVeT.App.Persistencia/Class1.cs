a2a38c3a23319edffd6f3071ce697c64936389ba
EDZA09 <eduarstiven2001@gmail.com>
2021-09-20 21:45:50 -0500

    Implementando la capa de Persistencia y generando migraciones

--- HosVet.App/HosVeT.App.Persistencia/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace HosVeT.App.Persistencia
-{
-    public class Class1
-    {
-    }
-}
---
+++ HosVet.App/HosVet.App.Consola/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using  HosVet.App.Dominio;
+using  HosVet.App.Persistencia;
+namespace HosVet.App.Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
