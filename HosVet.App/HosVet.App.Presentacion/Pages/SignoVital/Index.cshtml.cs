c697a5529b9e4b083dbb12b5ca20d5b6a08a1ec8
EDZA09 <eduarstiven2001@gmail.com>
2021-10-09 10:28:31 -0500

    a

--- HosVet.App/HosVet.App.Presentacion/Pages/SignoVital/Index.cshtml.cs
+++ HosVet.App/HosVet.App.Presentacion/Pages/SignoVital/Index.cshtml.cs
@@ -4,8 +4,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using HosVet.App.Persitencia;
-using HosVet.App.Dominio;
 
 namespace HosVet.App.Presentacion.Pages
 {
