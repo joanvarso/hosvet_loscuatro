61647d81a5d1a2590d70fa67b7cdbf4082cb9ce6
EDZA09 <eduarstiven2001@gmail.com>
2021-10-12 21:49:18 -0500

    Página Eliminar Mascota

--- HosVet.App/HosVet.App.Presentacion/Pages/Mascotas/Eliminar.cshtml.cs
+++ HosVet.App/HosVet.App.Presentacion/Pages/Mascotas/Eliminar.cshtml.cs
@@ -4,13 +4,22 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-
+using HosVet.App.Dominio;
+using HosVet.App.Persistencia;
 namespace HosVet.App.Presentacion.Pages.Mascotas
 {
+  private readonly IRepositorioMascota _repositorioMascota;
+  public Mascota mascota;
+
     public class EliminarModel : PageModel
     {
-        public void OnGet()
+      public EliminarModel(IRepositorioMascota _repositorioMascota)
+      {
+          this._repositorioMascota = _repositorioMascota;
+      }
+        public void OnGet(int id)
         {
+          mascota = _repositorioMascota.BorrarMascota(id);
         }
     }
 }
